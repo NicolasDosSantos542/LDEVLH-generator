2492879f58f4782138dbd8d0df61fce05b4c58c3
Demeyère Dos Santos Nicolas <dossantos.n@dxmprofuse.com>
2023-09-22 14:03:19 +0200

    check no parameters

--- backend/src/Controller/GameController.php
+++ backend/src/Controller/GameController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+ 
 
 
 class GameController extends AbstractController
@@ -27,12 +29,15 @@ class GameController extends AbstractController
         return new JsonResponse($output);
     }
 
-    
+
     #[Route('/game', name: 'create_game', methods: ['POST'])]
     public function createGame(EntityManagerInterface $em, ValidatorInterface $validator, Request $request): JsonResponse
     {
+       
         $parameters = json_decode($request->getContent(), false);
-
+        if(!$parameters){
+            throw new HttpException('Invalid parameters');
+        }
         $game = new Game();
         $game->setName($parameters->name);
         $game->setCreatorId($parameters->user_id);
@@ -43,12 +48,20 @@ class GameController extends AbstractController
             return new JsonResponse((string) $errors, 400);
         }
 
-        // tell Doctrine you want to (eventually) save the User (no queries yet)
         $em->persist($game);
 
         // actually executes the queries (i.e. the INSERT query)
         $em->flush();
 
         return new JsonResponse(['message' => 'Saved new game with id ' . $game->getId(), "data" => $game]);
+    }
+
+    #[Route('game/{id}', methods:['PUT'], name : "update_game")]
+    public function updateGame(EntityManagerInterface $em, ValidatorInterface $validator, Request $request){
+        $parameters = json_decode($request->getContent(), false);
+        if(!$parameters){
+            throw new HttpException('Invalid parameters');
+        }
+
     }
 }
